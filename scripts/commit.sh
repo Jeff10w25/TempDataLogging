059c93e0b332ea9083800c24574a414bec13b024
Thanathorn <thanatorns.2545@gmail.com>
2024-02-26 21:10:46 +0700

    Regular auto-commit at 10:30 PM

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-cd TempDataLogging/Excel
+cd TempDataLogging/excel
 git add --all
 
 timestamp() {
   date +"at 10:30 PM"
 }
 
+git config --global --unset htpps.proxy
 git commit -am "Regular auto-commit $(timestamp)"
 git push origin main
