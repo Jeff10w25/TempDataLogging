9c1fc42414b14c88dc10053a4e5a126990526952
Thanathorn <thanatorns.2545@gmail.com>
2024-02-26 21:12:25 +0700

    Regular auto-commit at 10:30 PM

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -6,6 +6,6 @@ timestamp() {
   date +"at 10:30 PM"
 }
 
-git config --global --unset htpps.proxy
 git commit -am "Regular auto-commit $(timestamp)"
+git config --global --unset htpps.proxy
 git push origin main
